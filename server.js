8525fba3ff905df689e358459610dfd1021fddc4
Manoel <gal18021@byui.edu>
2022-05-13 13:43:49 -0600

    Update on class 05/13/22

--- server.js
+++ server.js
@@ -1,24 +1,28 @@
-const express = require('express'); //import the library
-const bodyParser = require('body-parser'); //body parser is called middleware
+const express = require('express'); //imports the library
+const bodyParser = require('body-parser'); //body parser is called middleware (special to call request from a client)
 const port = 3000; // defining which port is gonna be used
-const app = express(); //Use the library. creates and express a application
+const app = express(); //Use the library. We call the library. Creates and express a application
 // We are buildin an API // Building our own server on a computer. 
 
-app.use(bodyParser.json()); // use the middleware (cal it befor anything else happens on each request)
+app.use(bodyParser.json()); // use the middleware (cal it before anything else happens on each request)
 
-app.listen(port, ()=>{
+app.listen(port, ()=>{   // We are listening to an incoming request
     console.log("listening on port: "+port);
 }); //listen ///we are using a non standard port (not 443-secur or 80-nonsecure)//any port greater than a 1000 is pos
-
-
+// 
 app.get('/',(request,response)=>{
     response.send("Hello Manoel Galvao")
 }); //respond
 
 
 app.post('/login', (request, response)=>{ // a post request is used by a cliente to send a new info to an API
-    const loginRequest = request.body;
-    if (loginRequest.userName=="aba@google.com" && loginRequest.password=="123456@Manoel"){
+    const loginRequest = request.body; // check the data was sent. Request has a lot of data inside.
+    console.log("Request Body", JSON.stringify(request.body));
+    // search database for username, and retrive current password
+
+    // compare the hashed version of the password that was sent with the hashed version of the database
+
+    if (loginRequest.userName=="aba@google.com" && loginRequest.password=="123456@Manoel"){ // 
         response.status(200); // 200 means OKay
         response.send("Welcome");
     } else{
