54c276b89d6fc1b94e27e95ebc5cdf76ae3b827f
mgalvao2020 <72952177+mgalvao2020@users.noreply.github.com>
2022-06-24 13:15:38 -0600

    Update the https address

--- apiclient.bat
+++ apiclient.bat
@@ -1,20 +1,3 @@
-curl -d "@data.json" -X POST -H "Content-Type:application/json" http://localhost:3000/login
+curl -d "@data.json" -X POST -H "Content-Type:application/json" https://manoelnodejs.cit270.com/signup
 
-# curl gets the html from the page, calls APIs, a command line tool to traansfer data
-# -d tells that we will use data (-d)
-# Where data is
-# curl by defaul 
-# -X POST - its an command... could be push, get, delete, etc..
-# There is a list of 25 commands we can do in a HTTP protocol
-# -H Header - a flag that tells 
-# "Content-Type:"
-# The servers is listening for the "application/json"
-# http://localhost:3000/login it is the URL we're going to
-# .ps file is a power shell
-# 
-#
-#
-#
-#
-#
-#
+
